09c4a3037c5d7f9e46ee0835b223cb6763f77d63
eric wei <ewei337@hotmail.com>
2018-07-27 19:45:00 +1000

    delete ue4 folder

--- Eric/UE4/MyTCP/Intermediate/Build/Win64/MyTCP/Development/UELinkerFixups/PCH.UELinkerFixups.cpp
+++
@@ -1,1 +0,0 @@
-#include "D:/Eric/UE4.17/MyTCP/Intermediate/Build/Win64/MyTCP/Development/UELinkerFixups/PCH.UELinkerFixups.h"
--- Eric/UE4/MyTCP/Intermediate/Build/Win64/MyTCP/Development/UELinkerFixups/PCH.UELinkerFixups.h
+++
@@ -1,2 +0,0 @@
-// PCH for D:\Eric\UE4.17\MyTCP\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h
-#include "D:\Eric\UE4.17\MyTCP\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
